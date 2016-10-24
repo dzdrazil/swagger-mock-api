f1934c0340b359c067d60030fb6c718f06c5793c
Jónatan Núñez <jnunez@bebee.com>
2016-10-24 13:10:53 +0200

    search JSON type example strip JSON comments

--- src/MockData.js
+++ src/MockData.js
@@ -1,6 +1,9 @@
 'use strict';
 
+import stripJsonComments from 'strip-json-comments';
+
 import Parser from './Parsers/Parser'
+
 let parser = new Parser();
 
 export default function MockData(definition, configMock) {
@@ -8,20 +11,30 @@ export default function MockData(definition, configMock) {
 
   if (!schema) return null;
 
-  let mock = {};
-  if (configMock.useExamples) {
-    if (configMock.useExamples && definition.examples && definition.examples['application/json']) {
-      mock = definition.examples['application/json'];
-    }
+  let mock = null;
 
-    if (configMock.extendExamples) {
-      mock = Object.assign(parser.parse(schema), mock);
+  if (configMock.useExamples) {
+    if (configMock.useExamples && definition.examples) {
+      let keys = Object.keys(definition.examples);
+      let key = keys.find(function(obj) {
+        return obj.includes('json');
+      });
+      if (key) {
+        mock = definition.examples[key];
+        if (typeof(mock) === 'string') {
+          mock = stripJsonComments(mock);
+          mock = JSON.parse(mock);
+        }
+      }
+      if (configMock.extendExamples) {
+        mock = Object.assign(parser.parse(schema), mock);
+      }
     }
   }
-  else {
+
+  if (!mock) {
     mock = parser.parse(schema);
   }
-
 
   return mock;
 };
